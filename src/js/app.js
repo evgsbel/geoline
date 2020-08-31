f5cb74ee2de9cd9766c020035210c71114103a02
evgsbel <beloff163@gmail.com>
2020-09-01 02:31:17 +0400

    main mobile ver finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
-//select language
+//preloader
+$(() => {
+    setTimeout(function () {
+        $('.preloader__wrp').fadeOut();
+    }, 100);
+});
+//select languageф
 $('.language__link').hover(function () {
     $('.language__link').removeClass('is-active');
     $(this).toggleClass('is-active');
@@ -38,7 +44,7 @@ $(document).ready(function () {
             dotsClass: 'slider__dots',
             fade: true,
             cssEase: 'linear',
-            autoplay: true,
+            autoplay: false,
             autoplaySpeed: 5000,
         });
 
@@ -72,23 +78,62 @@ $(() => {
         });
     });
 
+    function checkWidth() {
+        let windowWidth = $('body').innerWidth()
+        if (windowWidth > 769) {
+            console.log('tessts')
+            $(function () {
+                var topPos = $('#aside1').offset().top;
 
-    $(function () {
-        var topPos = $('#aside1').offset().top;
+                $(window).scroll(function () {
+                    let top = $(document).scrollTop(),
+                        pip = $('.stop').offset().top,
+                        height = $('#aside1').outerHeight();
+                    if (top > topPos && top < pip - height) {
+                        $('#aside1').addClass('in-fix').removeAttr("style");
+                    } else if (top > pip - height) {
+                        $('#aside1').removeClass('in-fix').css({'position': 'absolute', 'bottom': '0'});
+                    } else {
+                        $('#aside1').removeClass('in-fix');
+                    }
+                });
+            });
+        }
+    }
 
-        $(window).scroll(function () {
-            let top = $(document).scrollTop(),
-                pip = $('.stop').offset().top,
-                height = $('#aside1').outerHeight();
-            if (top > topPos && top < pip - height) {
-                $('#aside1').addClass('in-fix').removeAttr("style");
-            } else if (top > pip - height) {
-                $('#aside1').removeClass('in-fix').css({'position': 'absolute', 'bottom': '0'});
-            } else {
-                $('#aside1').removeClass('in-fix');
-            }
-        });
+    checkWidth();
+    $(window).resize(function () {
+        checkWidth(); // проверит при изменении размера окна клиента
     });
 });
 
 
+$(() => {
+    $('.js-menu__open').on('click', function headerHambClick() {
+        $('body')
+            .toggleClass('menu-open')
+    });
+    $('.js-menu__close').on('click', function popupCloseBtnClick() {
+        $('body').removeClass('menu-open');
+    });
+    $('.js-open-mobile-sub').on('click', function openMobileSub() {
+        $('.js-open-mobile-sub').removeClass('is-open')
+        $(this).addClass('is-open')
+        $(this).find('a').removeAttr('href')
+        $('.js-open-mobile-sub').find('.sub__menu_mobile').slideUp(400)
+        $(this).find('.sub__menu_mobile').slideToggle(400)
+    })
+});
+
+
+//ancors
+$(() => {
+    $("a.js-ancor-link").click(function () {
+        let elementClick = $(this).attr("href")
+        let destination = $(elementClick).offset().top
+
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+        console.log(destination)
+        return false;
+    });
+});
--- src/js/scrollAnimations.js
+++ src/js/scrollAnimations.js
@@ -2,7 +2,10 @@ import TimelineMax from 'TimelineMax'
 import ScrollMagic from 'ScrollMagic'
 import 'animation.gsap'
 import 'debug.addIndicators'
-(function($) {
+
+
+(function ($) {
+    let tl0 = new TimelineMax()
 
     // Init ScrollMagic
     var ctrl = new ScrollMagic.Controller({
@@ -11,8 +14,9 @@ import 'debug.addIndicators'
         }
     });
 
+
     // Создаем сцену
-    $("#one").each(function() {
+    $("#one").each(function () {
 
         new ScrollMagic.Scene({
             triggerElement: this
@@ -22,5 +26,22 @@ import 'debug.addIndicators'
 
     });
 
+
 })(jQuery);
+
+$(function () {
+    let tl0 = new TimelineMax();
+    const controller = new ScrollMagic.Controller();
+    tl0
+        .fromTo('.preloader__logo', .4, {x: 10, opacity: 0}, {x: 0, opacity: 1}, 0.5)
+        .staggerFromTo('.preloader__caption', .3, {x: 10, opacity: 0}, {x: 0, opacity: 1}, 0.3)
+        .staggerFromTo('.preloader__subtitle', .3, {x:10,opacity:0},{x:0,opacity:1}, 0.3)
+        .staggerFromTo('.preloader', 1, {scale: 1}, {
+            scale: 1.1,
+            ease: Linear.easeNone,
+            repeatDelay: 0,
+            repeat: -1,
+            yoyo: true
+        }, 0)
+})
 
